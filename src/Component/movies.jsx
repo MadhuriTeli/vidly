3a4f8ca6b7f62bca2d15ca04a6e00f54e92cc83c
Madhuri <madhuriteli07@gmail.com>
2020-11-25 19:16:28 +0530

    commited...

--- src/Component/movies.jsx
+++ src/Component/movies.jsx
@@ -6,16 +6,22 @@ class Movies extends Component {
         movies: getMovies()
      }
 
-     handleDelete = movie => {
+    handleDelete = movie => {
+         //creats new array with all movies except movies passed to delete
          const movies = this.state.movies.filter(m =>m._id !== movie._id);
-         this.setState({ movies });
+        this.setState({ movies }); // passed this object to state movie object to assign new movie array
+        
      };
 
     render() { 
-        const {length: count} = this.state.movies;
-        if(count === 0)
+        //count = this.state.movies.length;  
+        const {length: count} = this.state.movies; //counts the total no. of movie in currently in movie objet 
+    //if count is 0 the print no movie
+    //else return total no of movies and print table
+ 
+        if (count === 0)
         return <p> There is no movie in this database</p>;
-       
+//if there are more than one element then we put all elements in one <div> or <React.Fragment>
        return (
        <React.Fragment>
            <p>Showing {count} movies in the database.</p>
@@ -27,7 +33,7 @@ class Movies extends Component {
                     <th>Title</th>
                     <th>Genre</th>
                     <th>Stock</th>
-                    <th>Pinky</th>
+                    <th>Rate</th>
                     <th></th>
                 </tr>
             </thead>
